fa572b32938dbe4a241cdaad87e8531e0e2de5b6
Seethalaksmi Ponnusamy <sponnusamy@sapient.com>
2018-02-23 12:46:04 +0530

    Mortgage Calculator

---
+++ MortgageCalculator/MortgageCalculator.Web/App_Start/AutofacConfig.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using MortgageCalculator.Web;
+using MortgageCalculator.Web.Helpers;
+using MortgageCalculator.Web.Provider;
+using System.Web.Mvc;
+
+namespace MortgageCalculator.Web
+{
+    public class AutofacConfig
+    {
+        public static void Initialize()
+        {
+            var builder = new ContainerBuilder();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(RegisterServices(builder)));
+        }
+
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterType<MortageApiProvider>().As<IMortageApiProvider>().InstancePerRequest();
+            builder.RegisterType<ApiClientHelper>().As<IApiClientHelper>()
+       .SingleInstance();
+            return builder.Build();
+        
+
+        }
+    }
+}

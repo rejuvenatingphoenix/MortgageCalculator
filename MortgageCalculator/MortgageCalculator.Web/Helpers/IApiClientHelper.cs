49a0f30c51f651a8516d95e2f7ac450a48f93d92
Seethalaksmi Ponnusamy <sponnusamy@sapient.com>
2018-02-23 12:57:42 +0530

    Mortgage Calculator

---
+++ MortgageCalculator/MortgageCalculator.Web/Helpers/IApiClientHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+
+namespace MortgageCalculator.Web.Helpers
+{
+    public interface IApiClientHelper
+    {
+        HttpClient HttpClient
+        {
+            get;
+        }
+    }
+}
